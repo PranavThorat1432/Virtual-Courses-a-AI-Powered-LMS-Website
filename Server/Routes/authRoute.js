3defa76380a95cde4f108a9a5fe53f2c6f451503
PranavThorat1432 <pranavthorat95@gmail.com>
2026-01-05 13:52:54 +0530

    authRoutes

---
+++ Server/Routes/authRoute.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { googleAuth, resetPassword, sendOTP, signIn, signOut, signUp, verifyOTP } from '../Controllers/authController.js';
+
+const authRouter = express.Router();
+
+authRouter.post('/signup', signUp);
+authRouter.post('/signin', signIn);
+authRouter.get('/signout', signOut);
+authRouter.post('/send-otp', sendOTP);
+authRouter.post('/verify-otp', verifyOTP);
+authRouter.post('/reset-password', resetPassword);
+authRouter.post('/google-auth', googleAuth);
+
+export default authRouter;
